a835625176dc19c66de3691f7235cdc0b495a010
kiramakayz <kiram751214095@gmail.com>
2023-07-18 18:35:31 +0300

    branch 3 trial

---
+++ js3.js
@@ -0,0 +1,8 @@
+const students = ['akram','kiram','mageye'];
+
+const value = document.querySelector('.value');
+
+console.log(value)
+let studentsMap = students.map(std => (std))
+
+value.textContent = studentsMap
